0be3a56131b9a65513a10f40350ea6b01a549205
Jeeson <smuwjs@163.com>
2017-08-28 12:19:34 +0800

    Init Commit.

--- arch/src/main/java/me/jeeson/android/samples/dagger2/arch/base/BaseFragment.java
+++ arch/src/main/java/me/jeeson/android/samples/dagger2/arch/base/BaseFragment.java
@@ -1,12 +1,15 @@
 package me.jeeson.android.samples.dagger2.arch.base;
 
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 
 import javax.inject.Inject;
 
+import dagger.android.AndroidInjection;
 import dagger.android.AndroidInjector;
 import dagger.android.DispatchingAndroidInjector;
+import dagger.android.support.AndroidSupportInjection;
 import dagger.android.support.HasSupportFragmentInjector;
 
 /**
@@ -20,7 +23,15 @@ public class BaseFragment extends Fragment implements HasSupportFragmentInjector
     DispatchingAndroidInjector<Fragment> childFragmentInjector;
 
     @Override
+    public void onAttach(Context context) {
+        AndroidSupportInjection.inject(this);
+        super.onAttach(context);
+    }
+
+    @Override
     public AndroidInjector<Fragment> supportFragmentInjector() {
         return childFragmentInjector;
     }
+
+
 }
